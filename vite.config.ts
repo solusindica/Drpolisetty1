116fa2927807b3a76cafe027903ef83ba68f04ec
solusindica <drprs@sgprs.com>
2026-01-13 13:10:17 +0530

    Cleanup gh-pages and configure Vite for GitHub Pages docs deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
+    build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
