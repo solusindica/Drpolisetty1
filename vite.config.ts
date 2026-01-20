4bf8227dabde8dd77fbd52e922d5ba70bf16b2c0
solusindica <drprs@sgprs.com>
2026-01-20 10:36:24 +0530

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-    build: {
-    outDir: 'docs',
-  },
+  base: "/",
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
